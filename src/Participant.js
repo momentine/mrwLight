e1111921082587eff502893cd1189b33f7023cda
Arun Kavishwar <arunkav@.brown.edu>
2022-11-27 18:36:50 -0500

    small but working functionality

--- src/Participant.js
+++ src/Participant.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
 
 
-const Participant = ({ participant }) => {
+const Participant = ({ participant, canvasBool, styles }) => {
   const [videoTracks, setVideoTracks] = useState([]);
   const [audioTracks, setAudioTracks] = useState([]);
 
@@ -73,21 +73,24 @@ const Participant = ({ participant }) => {
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
   useEffect(() => {
+    if (!canvasBool) {
+      return
+    }
     contextRef.current = canvasRef.current.getContext("2d");
     const constraints = {
-      video: { width: { min: 1280 }, height: { min: 720 } },
+      video: { width: { min: 20 }, height: { min: 20 } },
     };
+    // console.log(videoRef)
+    // console.log(videoRef.current)
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-  
-      videoRef.current.srcObject = stream;
+      // console.log("sdgjoi", stream)
+      // console.log("sdgjoi", videoRef.current)
+      // videoRef.current.srcObject = stream;
       sendToMediaPipe();
-      console.log(stream);
-    });
-    
-    // if (videoRef) {
-    //   sendToMediaPipe();
-    // }
-      
+     });
+     console.log(("hopsod["))
+  
+    // sendToMediaPipe();
     const selfieSegmentation = new SelfieSegmentation({
       locateFile: (file) =>
         `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`,
@@ -101,8 +104,8 @@ const Participant = ({ participant }) => {
     selfieSegmentation.onResults(onResults);
 
     const sendToMediaPipe = async () => {
-      if (videoRef && !videoRef.current.videoWidth) {
-        console.log(videoRef.current.videoWidth);
+      if (videoRef && videoRef.current && !videoRef.current.videoWidth) {
+        // console.log(videoRef.current.videoWidth);
         requestAnimationFrame(sendToMediaPipe);
       } else {
         await selfieSegmentation.send({ image: videoRef.current });
@@ -112,6 +115,9 @@ const Participant = ({ participant }) => {
   }, []);
 
   const onResults = (results) => {
+    if (!canvasBool) {
+      return
+    }
     contextRef.current.save();
     contextRef.current.clearRect(
       0,
@@ -127,17 +133,18 @@ const Participant = ({ participant }) => {
       canvasRef.current.height
     );
     // Only overwrite existing pixels.
-    contextRef.current.globalCompositeOperation = "source-out";
-    contextRef.current.fillStyle = "#00FF00";
-    contextRef.current.fillRect(
-      0,
-      0,
-      canvasRef.current.width,
-      canvasRef.current.height
-    );
+    // contextRef.current.globalCompositeOperation = "source-in";
+    // contextRef.current.fillStyle = "#00FF00";
+    // contextRef.current.fillRect(
+    //   0,
+    //   0,
+    //   canvasRef.current.width,
+    //   canvasRef.current.height
+    // );
+    
 
     // Only overwrite missing pixels.
-    contextRef.current.globalCompositeOperation = "destination-atop";
+    contextRef.current.globalCompositeOperation = "source-in";
     contextRef.current.drawImage(
       results.image,
       0,
@@ -146,14 +153,37 @@ const Participant = ({ participant }) => {
       canvasRef.current.height
     );
 
+    // flip hoirzontally?
+    contextRef.current.translate(640, 0);
+    contextRef.current.scale(-1, 1);
+
     contextRef.current.restore();
   };
+
+
+
+  // STACK ABSOLUTELY
+
+  // if canvas bool is true...we return no image just the cutout
+  // if canvas bool is false...we don't even bother with any of the normal screen stuff
   return (
+
+    canvasBool ? 
     <div className="participant">
       <h3>{participant.identity}</h3>
-      <video ref={videoRef} autoPlay={true} />
-      <audio ref={audioRef} autoPlay={true} muted={true} />
-      <canvas ref={canvasRef} width={1280} height={720} />
+      <div id="overlayBOX">
+        <video ref={videoRef} autoPlay={true} style={{filter:"opacity(0%)"}} />
+        <audio ref={audioRef} autoPlay={true} muted={false} />
+        <canvas style={{position:"absolute", top:"0", left:"0", width:"720px", height:"540px"}} ref={canvasRef} />
+      </div>
+    </div>
+    :
+    <div className="participant">
+      <h3>{participant.identity}</h3>
+      <div id="overlayBOX">
+        <video style={styles} ref={videoRef} autoPlay={true} />
+        <audio ref={audioRef} autoPlay={true} muted={false} />
+      </div>
     </div>
   );
 };
--- src/Room.js
+++ src/Room.js
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Participant from "./Participant";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 const Room = ({ roomName, room, handleLogout }) => {
   const [participants, setParticipants] = useState([]);
 
+  // const windowFrontBG = useState([])
+  // const windowFrontG = useState([])
+  const handle = useFullScreenHandle();
+
+  const [windowFrontBG, setWFBG] = useState([]);
+  const [windowBackBG, setWBBG] = useState([]);
+  const [windowFrontG, setWFG] = useState([]);
+
   useEffect(() => {
     const participantConnected = (participant) => {
+      // adds to remote
       setParticipants((prevParticipants) => [...prevParticipants, participant]);
+
+      if (participant.identity === "windowBack") {
+        setWFBG((prevParticipants) => [...prevParticipants, participant]);
+      } else if (participant.identity === "guest") {
+        setWFG((prevParticipants) => [...prevParticipants, participant]);
+      } else {
+        setWBBG((prevParticipants) => [...prevParticipants, participant]);
+      }
+
     };
 
     const participantDisconnected = (participant) => {
       setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+      setWFBG((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+      setWBBG((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+      setWFG((prevParticipants) =>
         prevParticipants.filter((p) => p !== participant)
       );
     };
@@ -24,39 +52,93 @@ const Room = ({ roomName, room, handleLogout }) => {
     };
   }, [room]);
 
-  console.log("AYOAYOAYO")
-  console.log(participants)
-
   const remoteParticipants = participants.map((participant) => (
-    <Participant key={participant.sid} participant={participant} />
+    <Participant key={participant.sid} participant={participant} canvasBool={false}/> // this should be FALSE!
   ));
 
   // const windowBackF = participants.filter(p => p.identity === "windowFront")
-  let windowBackPpl = []
-
-  for (let i = 0; i < participants.length; i++) {
-    console.log("asdasdasd", participants[i], participants[i].identity) 
-    if (participants[i].identity === "windowFront") {
-      windowBackPpl.push(
-        <Participant key={participants[i].sid} participant={participants[i]} />
-      )
-    }
-  }
+  // let windowBackPpl = []
+  // for (let i = 0; i < participants.length; i++) {
+  //   console.log("asdasdasd", participants[i], participants[i].identity) 
+  //   if (participants[i].identity === "windowFront") {
+  //     windowBackPpl.push(
+  //       <Participant key={participants[i].sid} participant={participants[i]} />
+  //     )
+  //   }
+  // }
 
   
   const returnDisplay = (room) => {
-    console.log("ASIYSDGIUSD")
+    // logic can be greatly simplified by just returning remote guests if the overlay case is not true
+    // however this allows a much greater amount of granularity
+
     if (room.localParticipant.identity === "windowFront") {
-      return <div className="remote-participants">{remoteParticipants}</div>
-    } else if (room.localParticipant.identity === "guest") {
-      return <div className="remote-participants">{remoteParticipants}</div>
+      if (windowFrontBG.length !== 0 && windowFrontG.length !== 0) {
+        // return overlay
+        const wb = windowFrontBG[0]
+        const gst = windowFrontG[0]
+        const styl = {position: "absolute", top:"0", left:"0"}
+
+        return <>
+          <Participant styles={styl} key={wb.sid} participant={wb} canvasBool={false}/>
+          <Participant styles={styl} key={gst.sid} participant={gst} canvasBool={true}/>
+        </>
+        
+      } else if (windowFrontBG.length === 0 && windowFrontG.length !== 0) {
+        // return just guest
+        const wfg = windowFrontG.map((participant) => (
+          <Participant key={participant.sid} participant={participant} canvasBool={false}/>
+        ));
+        return <div className="remote-participants">{wfg}</div>
+      } else{
+        // return windowBack
+        const wfg = windowFrontBG.map((participant) => (
+          <Participant key={participant.sid} participant={participant} canvasBool={false}/>
+        ));
+        return <div className="remote-participants">{wfg}</div>
+      }
     }
+
+    if (room.localParticipant.identity === "windowBack") {
+      if (windowBackBG.length !== 0 && windowFrontG.length !== 0) {
+        // return overlay
+        const wb = windowBackBG[0]
+        const gst = windowFrontG[0]
+        const styl = {position: "absolute", top:"0", left:"0"}
+        return <>
+          <Participant styles={styl} key={wb.sid} participant={wb} canvasBool={false}/>
+          <Participant styles={styl} key={gst.sid} participant={gst} canvasBool={true}/>
+        </>
+        
+      } else if (windowBackBG.length === 0 && windowFrontG.length !== 0) {
+        // return just guest
+        const wfg = windowFrontG.map((participant) => (
+          <Participant key={participant.sid} participant={participant} canvasBool={false}/>
+        ));
+        return <div className="remote-participants">{wfg}</div>
+      } else{
+        // return windowFront
+        const wfg = windowBackBG.map((participant) => (
+          <Participant key={participant.sid} participant={participant} canvasBool={false}/>
+        ));
+        return <div className="remote-participants">{wfg}</div>
+      }
+    }
+
+    //   return <div className="remote-participants">{remoteParticipants}</div>
+    // } else if (room.localParticipant.identity === "guest") {
+    //   return <div className="remote-participants">{remoteParticipants}</div>
+    // }
+    
     return <div className="remote-participants">{remoteParticipants}</div>
   }
 
   return (
     <div className="room">
       <h2>Room: {roomName}</h2>
+      <button onClick={handle.enter}>
+        Fullscreen
+      </button>
       <button onClick={handleLogout}>Log out</button>
       <div className="local-participant">
 
